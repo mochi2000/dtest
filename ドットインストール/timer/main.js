8427adbbecb19fd6b6b2dca9f18bb0a635a49cbd
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-22 02:33:11 +0900

    stop
    入力フォームで値を取得してたま取得時間を調整しようとしたが入力した値だけ取得できず保留

--- ドットインストール/timer/main.js
+++ ドットインストール/timer/main.js
@@ -1,5 +1,6 @@
 (function(){
   'use strict';
+  // var p = document.getElementById('point').value;
   var timer = document.getElementById('timer');
   var start = document.getElementById('start');
   var stop = document.getElementById('stop');
@@ -12,9 +13,14 @@
 
   let c1 = document.getElementById('c1') 
   let c2 = document.getElementById('c2') 
-  
+
   let t1 = document.getElementById('tama')
   let t2 = document.getElementById('tama2') 
+
+  let siro = 0;
+  let kuro = 0;
+
+  
 
   t2.style.display = 'none';
 
@@ -46,47 +52,87 @@
   function xxx(t) {
     let star = Math.floor(t / 1000);
     let b = document.getElementById('b')
+    let kazu = document.getElementById('kazu')
+    let kazu2 = document.getElementById('kazu2')
+
     // let c = document.getElementById('c')
 
     if(star % 10  === 0 && star % 60  === 0) {
+      kuro += 1;
       const p = document.createElement('p');
       let val = 'a';
       p.setAttribute("id",val);
       b.appendChild(p);
       p.textContent = "★" ;
+      kazu2.textContent = kuro;
     } else if(star % 10 === 0) {
+      siro +=1;
       const p = document.createElement('p');
       let val = 'a';
       p.setAttribute("id",val);
       
       b.appendChild(p);
       p.textContent = "☆" ;
+      kazu.textContent = siro;
     }
   }
 
   function xxxx(t) {
     let star = Math.floor(t / 1000);
     let b = document.getElementById('b')
+    let kazus = document.getElementById('kazus')
+    let kazus2 = document.getElementById('kazus2')
     // let c = document.getElementById('c')
 
-    if(star % 10  === 0 && star % 60  === 0) {
+    if(star % 20  === 0 && star % 120  === 0) {
+      kuro += 1;
       const p = document.createElement('p');
       let val = 'a';
       p.setAttribute("id",val);
       b.appendChild(p);
       p.textContent = "★" ;
-    } else if(star % 10 === 0) {
+      kazus2.textContent = kuro;
+    } else if(star % 20 === 0) {
+      siro +=1;
       const p = document.createElement('p');
       let val = 'a';
       p.setAttribute("id",val);
       
       b.appendChild(p);
       p.textContent = "☆" ;
+      kazus.textContent = siro;
     }
   }
 
-  function countUp(){
+  // function xxxxx(t) {
+  //   let star = Math.floor(t / 1000);
+  //   let b = document.getElementById('b')
+  //   let kazus = document.getElementById('kazus')
+  //   let kazus2 = document.getElementById('kazus2')
+  //   // let c = document.getElementById('c')
+  //   console.log(p)
 
+  //   if(star % p  === 0 && star % (p * 6)  === 0) {
+  //     kuro += 1;
+  //     const p = document.createElement('p');
+  //     let val = 'a';
+  //     p.setAttribute("id",val);
+  //     b.appendChild(p);
+  //     p.textContent = "★" ;
+  //     kazus2.textContent = kuro;
+  //   } else if(star % p === 0) {
+  //     siro +=1;
+  //     const p = document.createElement('p');
+  //     let val = 'a';
+  //     p.setAttribute("id",val);
+      
+  //     b.appendChild(p);
+  //     p.textContent = "☆" ;
+  //     kazus.textContent = siro;
+  //   }
+  // }
+
+  function countUp(){
       timerId = setTimeout(function(){
           
           elapsedTime = Date.now() - startTime + timeToadd;
@@ -95,16 +141,28 @@
           countUp();
           if(c1.checked === true){
             xxx(elapsedTime);
+            c2.style.display = 'none';
           } else {
             xxxx(elapsedTime);
+            c1.style.display = 'none';
           }
+          // if(p != null){
+          //   xxxxx(elapsedTime);
+          //   c1.style.display = 'none';
+          //   c2.style.display = 'none';
+          // } else if(c1.checked === true){
+          //   xxx(elapsedTime);
+          //   c2.style.display = 'none';
+          // } else {
+          //   xxxx(elapsedTime);
+          //   c1.style.display = 'none';
+          // }
       },1000);
   }
 
   start.addEventListener('click',function(){
 
       startTime = Date.now();
-
       countUp();
   });
 
@@ -124,6 +182,11 @@
 
       updateTimetText();
       
+      c1.style.display = 'inline';
+      c2.style.display = 'inline';
+      siro = 0;
+      kuro = 0;
+
       let dom = document.getElementById('a');
         if (dom) {
         dom.parentNode.removeChild(dom);
