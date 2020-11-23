0f7a556f18547613632a6567b62f114e402eda36
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-23 23:25:18 +0900

    Update main.js

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,4 +1,11 @@
 'use strict';
 
-console.log('he\nllo wor\tld');
-console.log('h' + 'w');
+console.log(10 + 3); //13
+console.log(10 - 3); //7
+console.log(10 * 3); //30
+console.log(10 / 3); //3.3333...
+console.log(10 % 3); //1
+console.log(10 ** 3); //1000
+
+console.log(2 + 3 * 10); //32
+console.log((10 + 3) * 2); //26
