28468f9cb46ad429c5e40c1e2dc17ff561770f98
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-24 21:19:22 +0900

    定数

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,11 +1,6 @@
 'use strict';
 
-console.log(10 + 3); //13
-console.log(10 - 3); //7
-console.log(10 * 3); //30
-console.log(10 / 3); //3.3333...
-console.log(10 % 3); //1
-console.log(10 ** 3); //1000
+const price = 150;
 
-console.log(2 + 3 * 10); //32
-console.log((10 + 3) * 2); //26
+console.log(price * 140);
+console.log(price * 160);
