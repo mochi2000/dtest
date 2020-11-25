b0eddc4b8403cd0bed57a46bcb7caf6c07fd2a2f
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-25 22:17:55 +0900

    Update main.js

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,16 +1,11 @@
 'use strict';
 
-const price = 1200;
+const score = 85;
 
-console.log(price > 1000);
-console.log(price < 1000);
-console.log(price >= 1000);
-console.log(price <= 1000);
-console.log(price === 1000);
-console.log(price !== 1000);
-
-
-//false - 0,null,undefined,'',false
-//true - それ以外
-console.log(Boolean(0));
-console.log(Boolean('hello'));
+if(score >= 80) {
+  console.log('Great');
+} else if(score >= 60) {
+  console.log('Good');
+} else {
+  console.log('ok');
+}
