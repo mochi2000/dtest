3f984ab63e9a05d3195aa4584a8d86655c45f46a
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-25 22:08:56 +0900

    Update main.js

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,16 +1,6 @@
 'use strict';
 
-let price = 500;
-
-// console.log(price * 140);
-// console.log(price * 160);
-
-price++;
-price += 100;
-price--;
-price -= 100;
-price *= 100;
-price /= 100;
-
-// console.log(price * 140);
-// console.log(price * 160);
+console.log(typeof 'hello');
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof null);
