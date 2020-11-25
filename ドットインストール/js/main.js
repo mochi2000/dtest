dcf9e5577387b7c6f40de09bd19ae5fec7535ed7
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-25 22:11:10 +0900

    Update main.js

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
-console.log(typeof 'hello');
-console.log(typeof 3);
-console.log(typeof true);
-console.log(typeof null);
+console.log('5' * 3);
+
+//+のみ文字列の連結
+console.log('5' + 3);
+//数字に変換
+console.log(parseInt('5') + 3);
