ced9d9a0545652b54c7ad595425ac53af9c5838e
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-26 19:33:45 +0900

    Update main.js

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const signal = 'red'
-
-switch (signal) {
-  case 'red':
-    console.log('stop');
-    break;
-  case 'yellow':
-    console.log('Caution');
-    break;
-  case 'green':
-    console.log('go');
-    break;
+for (let i = 1; i <= 10; i++) {
+  console.log(`hello ${i}`);
 }
