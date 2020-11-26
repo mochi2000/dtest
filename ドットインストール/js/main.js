464bfcb6bc4ed3a16ea91a2acb67061fc79ab964
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-26 23:26:16 +0900

    Update main.js

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
-let hp = 100;
-
-while (hp > 0) {
-  console.log(`${hp} HP left`);
-  hp -= 15;
+for(let i =1; i <= 10; i++) {
+  if(i % 3 === 0) {
+    continue;
+  }
+  if(i === 4) {
+    break;
+  }
+  console.log(i)
 }
-
-do {
-  console.log(`${hp} HP left`);
-  hp -= 15;
-} while (hp >0);
