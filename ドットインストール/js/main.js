5660c97dda2bc4b6245e2b8a1e0b112e5aa33f9e
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-28 23:41:41 +0900

    アロー関数

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const sum = function(a,b,c) {
-  return a + b + c ;
-};
+const sum = (a,b,c) => a + b + c ;
+
 
 const total = sum(1,2,3) + sum(1,2,3);
 console.log(total);
+
+
+const double = a => a * 2;
+
+console.log( double(12))
