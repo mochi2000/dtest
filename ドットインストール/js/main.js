00e795e6533c386ef7b5b5ceda09c1fbcaf82e9c
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-28 23:44:29 +0900

    スコープ

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const sum = (a,b,c) => a + b + c ;
+const x = 2;
+//　この定数はどこでも有効
+
+function f() {
+  const x = 1;
+  // この定数はこの処理でのみ有効
+  console.log(x);
+}
 
 
-const total = sum(1,2,3) + sum(1,2,3);
-console.log(total);
-
-
-const double = a => a * 2;
-
-console.log( double(12))
