06cf465704ec142c9738c342cb1f6194a14bfd35
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-11-28 23:19:50 +0900

    関数

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
-function showAd(message = 'ad') {
-  console.log(`${message}`);
+function sum(a,b,c) {
+  return a + b + c ;
+  console.log(a,b,c);
 }
 
-showAd('Header ad');
-console.log('aaa')
-showAd();
+sum(1,2,3);
+sum(3,4,5);
+
+const total = sum(1,2,3) + sum(3,4,5);
+
+console.log(total);
