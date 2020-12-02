ffd747a3f80d5d807affcb792d3849dc3aac91c9
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-12-02 23:17:12 +0900

    値の交換

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
 {
-  const otherScores = [10,20];
-  const scores = [80,90,40,70, ...otherScores];
-  
+  const scores = [80,90,40,70];
+  const [a,b, ...others] = scores
 
-  function sum(a,b) {
-    console.log(a + b);
-  }
-  sum(...otherScores);
+  let x = 30;
+  let y = 70;
+  [x,y] = [y,x];
+
+  
 }
