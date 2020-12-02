4d0238efc013c38076797f2955898f776c7c1b8a
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-12-02 23:19:52 +0900

    forEsch

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -2,11 +2,9 @@
 
 {
   const scores = [80,90,40,70];
-  const [a,b, ...others] = scores
 
-  let x = 30;
-  let y = 70;
-  [x,y] = [y,x];
-
-  
+  scores.forEach((score,index) => {
+    console.log(`Score: ${index}: ${score}`)
+  });
 }
+
