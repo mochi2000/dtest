1226ce5fc4728e505d4a88719486e583746cd3a9
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-12-05 22:48:16 +0900

    オブジェクト

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 {
-  const scores = [80,90,40,71];
-
-   const evenScores = scores.filter(score => score % 2 === 0);
-   console.log(evenScores);
+  const point = {
+    x:100, 
+    y:100,
+  };
 }
 
