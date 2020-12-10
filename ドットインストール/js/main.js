6dfbcf1eafdd0d48cfcb04f5edc0d473ebbc894c
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-12-10 22:51:08 +0900

    文字列取得

--- ドットインストール/js/main.js
+++ ドットインストール/js/main.js
@@ -1,18 +1,10 @@
 'use strict';
 
 {
-  // let x = [1,2];
-  // let y = x;
-  // x[0] = 5;
+  const str = 'hello';
 
-  // console.log(x);
-  // console.log(y);
-
-  let x = [1,2];
-  let y = [...x];
-  x[0] = 5;
-
-  console.log(x);
-  console.log(y);
+  
+  let s = str.substring(2, 4);
+  console.log(s);
 }
 
